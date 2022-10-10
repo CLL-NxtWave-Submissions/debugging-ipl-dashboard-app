6309f78c362048f20bf5b327e4ab7fb3b3155b1f
Surya T N <suryatej.nim@yahoo.com>
2022-10-10 06:47:11 +0000

    Removed the 'exact' keyword in Link child component instance that composes the TeamCard parent component

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -6,7 +6,7 @@ const TeamCard = props => {
 
   return (
     <li className="team-item">
-      <Link exact to={`/team-matches/${id}`} className="link-item">
+      <Link to={`/team-matches/${id}`} className="link-item">
         <img src={teamImageURL} alt={name} className="team-logo" />
         <p className="team-name">{name}</p>
       </Link>
